6595c44ad4fa1dc984f0b955226c5d49dca21c14
제영호 <jeayh97@naver.com>
2025-03-11 12:27:58 +0900

    변수명 간략화

--- week2/Review/2828.cpp
+++ week2/Review/2828.cpp
@@ -1,21 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int N, M, J, cnt, apple_position, basket_left = 1, basket_right;
+int N, M, J, cnt, p, l = 1, r;
 
 void solve(int pos){
-    basket_right = basket_left + (M - 1);
-    if(pos >= basket_left && pos <= basket_right) return;
-    else if(pos < basket_left){
-        cnt += (basket_left - pos);
-        basket_left = pos;
-        basket_right = basket_left + (M -1);
+    r = l + (M - 1);
+    if(pos >= l && pos <= r) return;
+    else if(pos < l){
+        cnt += (l - pos);
+        l = pos;
+        r = l + (M -1);
         return;
     }
     else{
-        cnt += (pos - basket_right);
-        basket_right = pos;
-        basket_left = basket_right - (M - 1);
+        cnt += (pos - r);
+        r = pos;
+        l = r - (M - 1);
         return;
     }
 }
@@ -24,8 +24,8 @@ int main(){
     cin >> N >> M;
     cin >> J;
     while(J--){
-        cin >> apple_position;
-        solve(apple_position);
+        cin >> p;
+        solve(p);
     }
     cout << cnt << '\n';
     return 0;
