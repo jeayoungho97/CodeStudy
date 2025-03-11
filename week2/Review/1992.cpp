72717f63078eea1b5fecbbb39869e4a947ff5d40
제영호 <jeayh97@naver.com>
2025-03-11 11:51:46 +0900

    불필요한 변수 flag 제거

--- week2/Review/1992.cpp
+++ week2/Review/1992.cpp
@@ -1,28 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int N, y, x, ret, flag, Map[65][65];
+int N, y, x, ret, Map[65][65];
 
 string quad(int y, int x, int size){
-    flag = 0;
     if(size == 1) return to_string(Map[y][x]);
     for(int i = y; i < y + size; i++){
         for(int j = x; j < x + size; j++){
             if(Map[i][j] != Map[y][x]){
-                flag = 1;
-                break;
+                string quad1 = quad(y, x, size/2);
+                string quad2 = quad(y, x + size/2, size/2);
+                string quad3 = quad(y + size/2, x, size/2);
+                string quad4 = quad(y + size/2, x + size/2, size/2);
+                return "(" + quad1 + quad2 + quad3 + quad4 + ")";
             }
-        }
-        if(flag == 1) break;
+        }    
     }
-    if(flag == 0) return to_string(Map[y][x]);
-
-    string quad1 = quad(y, x, size/2);
-    string quad2 = quad(y, x + size/2, size/2);
-    string quad3 = quad(y + size/2, x, size/2);
-    string quad4 = quad(y + size/2, x + size/2, size/2);
-
-    return "(" + quad1 + quad2 + quad3 + quad4 + ")";
+    return to_string(Map[y][x]);
 }
 
 int main(){
