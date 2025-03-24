ec7d1b3c7edf1dd522f22e6f623c418479885b19
제영호 <jeayh97@naver.com>
2025-03-24 17:19:53 +0900

    1st thought failed

---
+++ week3/G12851/12851.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int N, K, cnt, Min = 100000;
+int visited[100001];
+// 결과들을 담아놓는 벡터
+vector<int> ret;
+
+void solve(int N, int cnt){
+    if(N < 0 || N > 100000) return;
+    if(N == K){
+        ret.push_back(cnt);
+        Min = min(cnt, Min);
+    }
+    if(N < K){
+        solve(N + 1, cnt + 1);
+        solve(2 * N, cnt + 1);
+        solve(N - 1, cnt + 1);
+
+    }
+    if(N > K){
+        solve(N - 1, cnt + 1);
+    }
+}
+
+int main(){
+    cin >> N >> K;
+    solve(N, 0);
+    for(int i : ret){
+        if(i == Min) cnt++;
+    }
+    cout << Min << '\n';
+    cout << cnt << '\n';
+    return 0;
+}
