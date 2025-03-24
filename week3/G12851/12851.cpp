906d4c14896184557f22739c649832b5dfee9f19
제영호 <jeayh97@naver.com>
2025-03-24 17:32:13 +0900

    논리는 맞지만 시간초과

--- week3/G12851/12851.cpp
+++ week3/G12851/12851.cpp
@@ -1,12 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 int N, K, cnt, Min = 100000;
-int visited[100001];
+int visited[100000];
 // 결과들을 담아놓는 벡터
 vector<int> ret;
 
 void solve(int N, int cnt){
     if(N < 0 || N > 100000) return;
+    if(visited[N] && cnt > visited[N]) return;
+    visited[N] = cnt;
     if(N == K){
         ret.push_back(cnt);
         Min = min(cnt, Min);
@@ -15,7 +17,6 @@ void solve(int N, int cnt){
         solve(N + 1, cnt + 1);
         solve(2 * N, cnt + 1);
         solve(N - 1, cnt + 1);
-
     }
     if(N > K){
         solve(N - 1, cnt + 1);
