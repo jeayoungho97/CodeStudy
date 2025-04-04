7a40c0157145c5b430af857af4a637aaa829b119
제영호 <jeayh97@naver.com>
2025-04-04 16:04:57 +0900

    15684 revisited : check is not true?

--- week3/O15684/15684.cpp
+++ week3/O15684/15684.cpp
@@ -1,36 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
-int N, M, H, a, b, cnt;
-vector<pair<int,int>> v[10];
-
-bool cmp(pair<int,int> p1, pair<int,int> p2){
-    return p1.first < p2.first;
-}
+const int INF = 987654321;
+int N, M, H, a, b, ret = INF;
+bool visited[31][11];
 
 bool check(){
-    for(int line = 1; line < 10; line++){
+    for(int i = 1; i < N; i++){
         stack<int> stk;
-        sort(v[line].begin(), v[line].end(), cmp);
-        for(int i = 0; i < v[line].size(); i++){
-            if(stk.size() && stk.top() == v[line][i].second) stk.pop();
-            else stk.push(v[line][i].second);
+        for(int j = 0; j < H; j++){
+            if(stk.size()){
+                int t = stk.top();
+                if(visited[j][i]){
+                    if(t == i) stk.pop();
+                    else stk.push(i);
+                }
+                if(visited[j][i - 1]){
+                    if(t == i - 1) stk.pop();
+                    else stk.push(i - 1);
+                }
+            }
+            else{
+                if(visited[j][i]) stk.push(i);
+                if(visited[j][i - 1]) stk.push(i - 1);
+            }
         }
-        if(stk.size()) return false;
+        if(!stk.empty()) return false;
     }
     return true;
 }
 
+void go(int row, int cnt){
+    if(cnt > 3 || cnt >= ret) return;
+    if(check()){
+        ret = min(ret, cnt);
+        return;
+    }
+    for(int i = row; i <= H; i++){
+        for(int j = 1; j < N; j++){
+            if(visited[i][j] || visited[i][j - 1] || visited[i][j + 1]) continue;
+            visited[i][j] = true;
+            go(i, cnt + 1);
+            visited[i][j] = false;
+        }
+    }
+}
+
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     cin >> N >> M >> H;
     for(int i = 0; i < M; i++){
         cin >> a >> b;
-        v[b].push_back({a,b});
-        if(b != 1) v[b-1].push_back({a,b});
+        visited[a][b] = true;
     }
-    if(check()){
-        cout << "YES\n";
-        return 0;
-    }
-    cout << "No\n";
+    go(1, 0);
+    cout << ((ret == INF) ? -1 : ret) << "\n";
     return 0;
 }
