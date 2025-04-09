62a859df44bb18013c6cafe50c1db506f5c5a847
제영호 <jeayh97@naver.com>
2025-04-10 06:39:42 +0900

    구글 스타일 가이드 대로 수정, 전위 증가에 대한 이해

--- self_study/동적할당/2577.cpp
+++ self_study/동적할당/2577.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
 #include <string>
-#include <algorithm>
+#include <vector>
 
 int main(){
-    int a, b, c;
-    std::cin >> a >> b >> c;
+  int a, b, c;
+  std::cin >> a >> b >> c;
 
-    std::string result = std::to_string(a * b * c);
+  int product = a * b * c;
+  std::string productStr = std::to_string(product);
 
-    for(int i = 0; i < 10; i++){
-        std::cout << std::count(result.begin(), result.end(), i + '0') << '\n';
-    }
+  std::vector<int> digitCount(10, 0);
 
-    return 0;
+  for (char digit : productStr) {
+      ++digitCount[digit - '0']; // 전위증가가 후위증가보다 더욱 명확한 의미를 가짐 
+}                                // 후위증가는 클래스, 반복자, 스마트 포인터 같은 객체에서는 성능 차이가 남.
+
+  for (int i = 0; i < 10; i++) {
+    std::cout << digitCount[i] << '\n';
+  }
+
+  return 0;
 }
