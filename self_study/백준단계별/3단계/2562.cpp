d67534aa26dfe38258b4fd972e032a500debb88c
제영호 <jeayh97@naver.com>
2025-04-10 09:57:58 +0900

    google style guide 에 맞게 양식 수정

--- self_study/백준단계별/3단계/2562.cpp
+++ self_study/백준단계별/3단계/2562.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
 #include <algorithm>
 
-int main(){
-    int idx, arr[9], result = 0;
-    for (int i = 0; i < 9; ++i) {
-        std::cin >> arr[i];
-        if(result < arr[i]){
-            result = arr[i];
-            idx = i + 1;
-        }
-    }
+int main()
+{
+  int max_index = 0;
+  int max_value = 0;
+  int numbers[9];
 
-    std::cout << result << '\n';
-    std::cout << idx << '\n';
-    return 0;
+  for (int i = 0; i < 9; ++i) {
+    std::cin >> numbers[i];
+    if (numbers[i] > max_value) {
+      max_value = numbers[i];
+      max_index = i + 1;
+    }
+  }
+
+  std::cout << max_value << '\n';
+  std::cout << max_index << '\n';
+  
+  return 0;
 }
