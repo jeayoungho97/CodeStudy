7f297ee52653c999a088324ff6dac2a1bdf0aff5
제영호 <jeayh97@naver.com>
2025-04-21 18:04:52 +0900

    1644 solved by optimizing prime number generator

--- week5/G1644.cpp
+++ week5/G1644.cpp
@@ -1,27 +1,35 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 #include <queue>
+#include <algorithm>
+
+std::vector<int> get_primes(int n) {
+  std::vector<bool> is_prime(n + 1, true);
+  is_prime[0] = is_prime[1] = false;
+
+  for (int i = 2; i * i <= n; ++i) {
+    if (is_prime[i]) {
+      for (int j = i * i; j <= n; j += i) {
+        is_prime[j] = false;
+      }
+    }
+  }
+
+  std::vector<int> primes;
+  for (int i = 2; i <= n; ++i) {
+    if (is_prime[i]) {
+      primes.push_back(i);
+    }
+  }
+
+  return primes;
+}
 
 int main() {
   int n;
   std::cin >> n;
 
-  if (n == 1) {
-    std::cout << 0 << '\n';
-    return 0;
-  }
-
-  std::vector<int> prime_numbers = {2};
-  for (int i = 3; i <= n; ++i) {
-    bool is_prime = true;
-    for (int a : prime_numbers) {
-      if (i % a == 0) {
-        is_prime = false;
-      }
-    }
-    if(is_prime) prime_numbers.push_back(i);
-  }
+  std::vector<int> prime_numbers = get_primes(n);
 
   std::sort(prime_numbers.begin(), prime_numbers.end(), std::greater<int>());
   int size = prime_numbers.size();
@@ -30,7 +38,7 @@ int main() {
   int sum = 0;
   std::priority_queue<int> pq;
 
-  for(int i = 0; i < size; ++i) {
+  for (int i = 0; i < size; ++i) {
     int here = prime_numbers[i];
     sum += here;
     pq.push(here);
@@ -49,4 +57,5 @@ int main() {
   std::cout << result << '\n';
 
   return 0;
+
 }
