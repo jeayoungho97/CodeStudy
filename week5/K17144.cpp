3b1271618bc0454d7c716add834a6b87a7a22a54
제영호 <jeayh97@naver.com>
2025-04-26 04:39:46 +0900

    refactoring 을 해서 코드는 좀 더 아름다워졌으나 성능은 오히려 약간 느려짐

--- week5/K17144.cpp
+++ week5/K17144.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
+#include <vector>
 
 int map[51][51];
 int r, c, t;
 int purifier_index;
+
 constexpr int di[4] = {-1, 0, 1, 0};
 constexpr int dj[4] = {0, 1, 0, -1};
 
-void spread_and_purify() {
+std::vector<std::pair<int, int>> path1, path2;
+
+constexpr int di_path1[4] = {0, -1, 0, 1};
+constexpr int dj_path1[4] = {1, 0, -1, 0};
+
+constexpr int di_path2[4] = {0, 1, 0, -1};
+constexpr int dj_path2[4] = {1, 0, -1, 0};
+
+void spread() {
   int next_add[51][51] = {{0}};
 
   for (int i = 0; i < r; ++i) {
@@ -35,36 +45,38 @@ void spread_and_purify() {
       map[i][j] += next_add[i][j];
     }
   }
+}
 
-  //공기청정기 상단부
-  for (int i = purifier_index - 2; i > 0; --i) {
-    map[i][0] = map[i - 1][0];
-  }
-  for (int i = 0; i < c - 1; ++i) {
-    map[0][i] = map[0][i + 1];
-  }
-  for (int i = 0; i < purifier_index - 1; ++i) {
-    map[i][c - 1] = map[i + 1][c - 1];
-  }
-  for (int i = c - 2; i > 0; --i) {
-    map[purifier_index - 1][i + 1] = map[purifier_index - 1][i];
-  }
-  map[purifier_index - 1][1] = 0;
+std::vector<std::pair<int, int>> make_path(int sy, const int dy[], const int dx[]) {
+  std::vector<std::pair<int, int>> path;
+  int count = 0;
+  int y = sy;
+  int x = 0;
+  
+  while(true) {
+    int ny = y + dy[count];
+    int nx = x + dx[count];
 
-  //공기청정기 하단부
-  for (int i = purifier_index + 1; i < r - 1; ++i) {
-    map[i][0] = map[i + 1][0];
+    if (ny == sy && nx == 0) break;
+    if (ny < 0 || nx <0 || ny >= r || nx >= c) {
+      ++count;
+      ny = y + dy[count];
+      nx = x + dx[count];
+    }
+    if (ny == sy && nx == 0) break;
+    y = ny;
+    x = nx;
+
+    path.push_back({ny, nx});
   }
-  for (int i = 0; i < c - 1; ++i) {
-    map[r - 1][i] = map[r - 1][i + 1];
+  return path;
+}
+
+void purify(std::vector<std::pair<int, int>> path) {
+  for (int i = path.size() - 1; i > 0; --i) {
+    map[path[i].first][path[i].second] = map[path[i - 1].first][path[i - 1].second];
   }
-  for (int i = r - 1; i > purifier_index; --i) {
-    map[i][c - 1] = map[i - 1][c - 1];
-  }
-  for (int i = c - 2; i > 0; --i) {
-    map[purifier_index][i + 1] = map[purifier_index][i];
-  }
-  map[purifier_index][1] = 0;
+  map[path[0].first][path[0].second] = 0;
 }
 
 int main() {
@@ -79,8 +91,13 @@ int main() {
     }
   }
 
+  path1 = make_path(purifier_index - 1, di_path1, dj_path1);
+  path2 = make_path(purifier_index, di_path2, dj_path2);
+
   while (t--) {
-    spread_and_purify();
+    spread();
+    purify(path1);
+    purify(path2);
   }
 
   int result = 0;
