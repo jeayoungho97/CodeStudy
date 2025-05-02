b90fa5244d4a156cf962f0b2f67545e323de66b0
제영호 <jeayh97@naver.com>
2025-05-02 15:05:34 +0900

    refactored

--- self_study/백준단계별/10단계/3009.cpp
+++ self_study/백준단계별/10단계/3009.cpp
@@ -1,23 +1,13 @@
 #include <iostream>
-#include <map>
 
 int main() {
-  std::map<int, int> counter[2];
+  int x = 0, y = 0;
   for (int i = 0; i < 3; ++i) {
-    int x, y;
-    std::cin >> x >> y;
-    ++counter[0][x];
-    ++counter[1][y];
+    int a, b;
+    std::cin >> a >> b;
+    x ^= a;
+    y ^= b;
   }
-
-  int x, y;
-  for (auto a : counter[0]) {
-    if (a.second == 1) x = a.first;
-  }
-  for (auto a : counter[1]) {
-    if (a.second == 1) y = a.first;
-  }
-
   std::cout << x << ' ' << y << '\n';
   return 0;
 }
