85e707e74f1b201901f2ab310889856a6a95df27
제영호 <jeayh97@naver.com>
2025-05-07 18:54:16 +0900

    2632 solved

--- week5/V2632.cpp
+++ week5/V2632.cpp
@@ -2,44 +2,43 @@
 #include <vector>
 #include <unordered_map>
 
-int main() {
-  int needs, N, M;
-  std::cin >> N >> M;
+std::unordered_map<int, int> get_sub_sum_counts(const std::vector<int>& pizza) {
+  std::unordered_map<int, int> count_map;
+  int len = pizza.size();
+  std::vector<int> extended(pizza.begin(), pizza.end());
+  extended.insert(extended.end(), pizza.begin(), pizza.end());
+  
+  for (int size = 1; size < len; ++size) {
+    int sum = 0;
+    for (int i = 0; i < size; ++i) sum += extended[i];
+    ++count_map[sum];
 
-  std::vector<int> A(N), B(N);
+    for (int i = 1; i < len; ++i) {
+      sum = sum - extended[i - 1] + extended[i + size - 1];
+      ++count_map[sum];
+    }
+  }
+
+  int total = 0;
+  for (int v : pizza) total += v;
+  ++count_map[total];
+
+  return count_map;
+}
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  
+  int needs, N, M;
+  std::cin >> needs >> N >> M;
+
+  std::vector<int> A(N), B(M);
   for (int i = 0; i < N; ++i) std::cin >> A[i];
   for (int i = 0; i < M; ++i) std::cin >> B[i];
 
-  auto get_sub_sums = [](const std::vector<int>& pizza) {
-    std::vector<int> sums;
-    int len = pizza.size();
-    std::vector<int> extended(pizza.begin(), pizza.end());
-    extended.insert(extended.end(), pizza.begin(), pizza.end());
-
-    for (int size = 1; size < len; ++size) {
-      int sum = 0;
-      for (int i = 0; i < size; ++i) sum += extended[i];
-      sums.push_back(sum);
-
-      for (int i = 1; i < len; ++i) {
-        sum = sum - extended[i - 1] + extended[i + size - 1];
-        sums.push_back(sum);
-      }
-    }
-
-    int total = 0;
-    for (int v : pizza) total += v;
-    sums.push_back(total);
-
-    return sums;
-  };
-
-  auto a_sums = get_sub_sums(A);
-  auto b_sums = get_sub_sums(B);
-
-  std::unordered_map<int, int> a_count, b_count;
-  for (int s : a_sums) ++a_count[s];
-  for (int s : b_sums) ++b_count[s];
+  auto a_count = get_sub_sum_counts(A);
+  auto b_count = get_sub_sum_counts(B);
 
   int result = 0;
 
