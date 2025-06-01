f0b3b96b86841db3ce6ffc7a7fef0853cc68b790
제영호 <jeayh97@naver.com>
2025-06-01 20:30:44 +0900

    17070 복습

--- week7/17070.cpp
+++ week7/17070.cpp
@@ -11,22 +11,19 @@ int go(int i, int j, int dir) {
   if (ret != -1) return ret;
   ret = 0;
 
-  // 가로
   if (dir == 0 || dir == 1) {
     if (j + 1 < n && map[i][j + 1] == 0) {
       ret += go(i, j + 1, 0);
     }
   }
 
-  // 세로
   if (dir == 1 || dir == 2) {
     if (i + 1 < n && map[i + 1][j] == 0) {
       ret += go(i + 1, j, 2);
     }
   }
 
-  // 대각선
-  if (i + 1 < n && j + 1 < n) {
+  if (i < n - 1 && j < n - 1) {
     if (map[i + 1][j] == 0 && map[i][j + 1] == 0 && map[i + 1][j + 1] == 0) {
       ret += go(i + 1, j + 1, 1);
     }
@@ -46,5 +43,6 @@ int main() {
   std::memset(dp, -1, sizeof(dp));
 
   std::cout << go(0, 1, 0) << '\n';
+
   return 0;
 }
