33846ee9f4eaa9dca236c4f6839bf461f709262e
제영호 <jeayh97@naver.com>
2025-06-19 22:11:45 +0900

    solved with list

--- self_study/solved.ac/class2/11866.cpp
+++ self_study/solved.ac/class2/11866.cpp
@@ -1,36 +1,73 @@
+// #include <iostream>
+// #include <queue>
+// #include <vector>
+
+// int main() {
+//   int n, k;
+//   std::cin >> n >> k;
+
+//   std::queue<int> q;
+
+//   for (int i = 1; i <= n; ++i) {
+//     q.push(i);
+//   }
+
+//   std::vector<int> ret;
+//   while (q.size()) {
+//     for (int i = 0; i < k - 1; ++i) {
+//       q.push(q.front());
+//       q.pop();
+//     }
+
+//     ret.push_back(q.front());
+//     q.pop();
+//   }
+
+//   std::string final_ret = "<";
+//   for (int i : ret) {
+//     final_ret += std::to_string(i) + ", ";
+//   }
+//   final_ret.pop_back();
+//   final_ret.pop_back();
+//   final_ret += ">\n";
+
+//   std::cout << final_ret;
+//   return 0;
+// }
+
 #include <iostream>
-#include <queue>
+#include <list>
 #include <vector>
 
 int main() {
   int n, k;
   std::cin >> n >> k;
 
-  std::queue<int> q;
-
+  std::list<int> l;
   for (int i = 1; i <= n; ++i) {
-    q.push(i);
+    l.push_back(i);
   }
 
-  std::vector<int> ret;
-  while (q.size()) {
+  std::vector<int> result;
+  auto it = l.begin();
+
+  while(!l.empty()) {
     for (int i = 0; i < k - 1; ++i) {
-      q.push(q.front());
-      q.pop();
+      ++it;
+      if (it == l.end()) it = l.begin();
     }
 
-    ret.push_back(q.front());
-    q.pop();
+    result.push_back(*it);
+    it = l.erase(it);
+    if (it == l.end()) it = l.begin();
   }
 
-  std::string final_ret = "<";
-  for (int i : ret) {
-    final_ret += std::to_string(i) + ", ";
+  std::cout << "<";
+  for (int i = 0; i < result.size(); ++i) {
+    std::cout << result[i];
+    if (i != result.size() - 1) std::cout << ", ";
   }
-  final_ret.pop_back();
-  final_ret.pop_back();
-  final_ret += ">\n";
+  std::cout << ">\n";
 
-  std::cout << final_ret;
   return 0;
 }
