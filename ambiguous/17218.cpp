6957a93f6726fce24665642e2e5609273de9997a
제영호 <jeayh97@naver.com>
2025-07-13 14:02:15 +0900

    Refactored : 17218 : Help by GPT

--- ambiguous/17218.cpp
+++ ambiguous/17218.cpp
@@ -1,37 +1,46 @@
 #include <iostream>
+#include <vector>
 #include <string>
-
-std::string str1, str2;
-std::pair<int, std::string> dp[41][41];
-
-std::pair<int, std::string> go(int i, int j) {
-  std::pair<int, std::string> result = {0, ""};
-
-  for (int k = i; k < str1.size(); ++k) {
-    for (int l = j; l < str2.size(); ++l) {
-      if (str1[k] == str2[l]) {
-        std::pair<int,std::string> &temp = dp[k][l];
-        if (temp.first > result.first) {
-          result = temp;
-        }
-        else {
-          auto next = go(k + 1, l + 1);
-          temp.first = next.first + 1;
-          temp.second = str1[k] + next.second;
-          if (temp.first > result.first) {
-            result = temp;
-          }
-        }
-      }
-    }
-  }
-
-  return result;
-}
+#include <algorithm>
 
 int main() {
-  std::cin >> str1 >> str2;
+    std::string str1, str2;
+    std::cin >> str1 >> str2;
 
-  std::cout << go(0, 0).second << '\n';
-  return 0;
+    int n = str1.size();
+    int m = str2.size();
+
+    // dp[i][j]: str1[0..i-1], str2[0..j-1]의 LCS 길이
+    std::vector<std::vector<int>> dp(n + 1, std::vector<int>(m + 1, 0));
+
+    // DP 테이블 채우기
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            if (str1[i] == str2[j]) {
+                dp[i + 1][j + 1] = dp[i][j] + 1;
+            } else {
+                dp[i + 1][j + 1] = std::max(dp[i][j + 1], dp[i + 1][j]);
+            }
+        }
+    }
+
+    // LCS 복원
+    std::string lcs;
+    int i = n, j = m;
+    while (i > 0 && j > 0) {
+        if (str1[i - 1] == str2[j - 1]) {
+            lcs += str1[i - 1];
+            --i;
+            --j;
+        } else if (dp[i - 1][j] > dp[i][j - 1]) {
+            --i;
+        } else {
+            --j;
+        }
+    }
+
+    std::reverse(lcs.begin(), lcs.end());
+    std::cout << lcs << '\n';
+
+    return 0;
 }
